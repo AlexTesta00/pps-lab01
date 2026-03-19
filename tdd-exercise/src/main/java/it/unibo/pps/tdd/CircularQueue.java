bb2a6a922dedd09b6e2e578be23cf319bc2f086c
Alex Testa <alex.testa@studio.unibo.it>
2026-03-19 11:22:26 +0100

    feat(CircularQueue): added CircularQueue Interface

--- tdd-exercise/src/main/java/it/unibo/pps/tdd/CircularQueue.java
+++ tdd-exercise/src/main/java/it/unibo/pps/tdd/CircularQueue.java
@@ -13,4 +13,49 @@ package it.unibo.pps.tdd;
  *   - Introduce methods in the interface in order to make the tests pass.
  *   - Refactor
  */
-public interface CircularQueue { }
+public interface CircularQueue {
+
+    /**
+     * Adds an element to the queue.
+     * If the queue is full, the oldest element is overwritten.
+     *
+     * @param element the element to add
+     */
+    void enqueue(int element);
+
+    /**
+     * Removes and returns the oldest element in the queue.
+     *
+     * @return the oldest element
+     * @throws IllegalStateException if the queue is empty
+     */
+    int dequeue();
+
+    /**
+     * Returns the oldest element without removing it.
+     *
+     * @return the oldest element
+     * @throws IllegalStateException if the queue is empty
+     */
+    int peek();
+
+    /**
+     * @return the current number of elements in the queue
+     */
+    int size();
+
+    /**
+     * @return the maximum capacity of the queue
+     */
+    int capacity();
+
+    /**
+     * @return true if the queue is empty
+     */
+    boolean isEmpty();
+
+    /**
+     * @return true if the queue is full
+     */
+    boolean isFull();
+}
